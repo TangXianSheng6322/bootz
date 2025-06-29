8faad538b50ad6abfbad3bad12aef20967b2f474
Nick <n.sakharov@icloud.com>
2025-06-29 17:10:41 +0700

    second section is done for mobile

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { LogoLineComponent } from '../logo-line/logo-line.component';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [LogoLineComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
---
+++ src/app/logo-line/logo-line.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-line',
+  imports: [],
+  templateUrl: './logo-line.component.html',
+  styleUrl: './logo-line.component.css'
+})
+export class LogoLineComponent {
+
+}
